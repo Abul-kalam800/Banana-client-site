e7f4cdb6b6c627b7dee60ffce6113b2ca48497b7
abul kalam <bd.abulkalam800@gamil.com>
2025-05-22 20:04:25 +0300

    added regsiter page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,27 +1,64 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import { AuthContex } from "../provider/AuthContex";
+import Swal from "sweetalert2";
 
 const Register = () => {
+  const { error, setError } = useState([]);
   const { creatUser } = use(AuthContex);
   const handleRegister = (e) => {
     e.preventDefault();
     const form = e.target;
     const formData = new FormData(form);
-    const email = formData.get('email')
-    const password = formData.get('password')
-    console.log(email,password)
 
+    const name = formData.get("name");
+    const phto = formData.get("photo");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    console.log(email, password, name, phto);
+
+    // password validation
+    if (password.length < 8) {
+      alert("Password must be 8 charectors");
+      return;
+    }
+
+    if (!/[A-Z]/.test(password)) {
+      alert("Must include at least one uppercase letter.");
+      return;
+    }
+    if (!/[a-z]/.test(password)) {
+      alert("Must include at least one lowercase letter.");
+      return;
+    }
+    if (!/[0-9]/.test(password)) {
+      alert("Must include at least one number.");
+      return;
+    }
+    if (!/[!@#$%^&*]/.test(password)) {
+      alert("Must include at least one special character (!@#$%^&*).");
+    }
+    elrs;
+    setError(error);
 
     creatUser(email, password)
       .then((result) => {
         console.log(result.user);
+
+        // aleart successfully
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Your work has been saved",
+          showConfirmButton: false,
+          timer: 1500,
+        });
       })
       .catch((error) => {
         console.log(error.message);
       });
   };
   return (
-    <div className="hero bg-base-200 min-h-screen">
+    <div className="hero bg-base-200">
       <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl p-4">
         <h1 className="text-3xl font-semibold text-center">Register Now!</h1>
         <div className="card-body">
@@ -35,6 +72,7 @@ const Register = () => {
               name="name"
               className="input"
               placeholder="Enter Your Name"
+              required
             />
             <label className="label">Email</label>
             <input
@@ -49,6 +87,7 @@ const Register = () => {
               name="photo"
               className="input"
               placeholder="Photo URL"
+              required
             />
 
             <label className="label">Password</label>
@@ -57,6 +96,7 @@ const Register = () => {
               name="password"
               className="input"
               placeholder="Password"
+              required
             />
             <div>
               <a className="link link-hover">Forgot password?</a>
